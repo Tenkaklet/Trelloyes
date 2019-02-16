d0eb4adf4a30d05266b3861aa8b3cf5ade9fa564
Max Carlquist <tenkaklet@gmail.com>
2019-02-16 13:29:23 +0100

    refined test file

--- src/Card.test.js
+++ src/Card.test.js
@@ -6,11 +6,11 @@ import renderer from 'react-test-renderer';
 describe('The Cards Component', () => {
     it('Renders without Crashing',  () => {
         const div = document.createElement('div');
-        ReactDOM.render(<Card />, div);
+        ReactDOM.render(<Card title="title test" content="content" />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
     it('Renders the UI as expected', () => {
-        const tree = renderer.create(<Card />).toJSON();
+        const tree = renderer.create(<Card title="title test" content="content" />).toJSON();
         expect(tree).toMatchSnapshot();
     });
 });
